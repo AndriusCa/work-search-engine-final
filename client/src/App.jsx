1b831d4bc01f90adb64ec3034222cd265c7b6833
Andrius <andriuscanis@gmail.com>
2023-08-23 22:52:19 +0300

    basic layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ContextWrapper } from "./context/GlobalContext";
+import { PublicLayout } from "./layout/PublicLayout";
+import { Home } from "./pages/Home";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import { Page404 } from "./pages/Page404";
+
+
 function App() {
   return (
-    <>APP CONTENT</>
-
-  );
+    <ContextWrapper>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={PublicLayout}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Route>
+          {/* <Route Component={UserLayout}>
+            <Route path='/dashboard' element={}></Route>
+          </Route> */}
+          <Route Component={PublicLayout}>
+            <Route path="*" element={<Page404 />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ContextWrapper>
+  )
 }
 
 export default App;
---
+++ client/src/layout/PublicLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+export function PublicLayout() {
+  return (
+    <>
+      <header>HEADER</header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>FOOTER</footer>
+    </>
+  )
+}
