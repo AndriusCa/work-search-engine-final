dc68e3a430c92c89d7dc01377ad1bdf104e09b72
Andrius <andriuscanis@gmail.com>
2023-09-08 19:26:36 +0300

    api

--- server/api/api.js
+++ server/api/api.js
@@ -4,6 +4,9 @@ import { login } from "./login.js";
 import { logout } from "./logout.js";
 import { jobTypes } from "./job-types.js"
 import { users } from "./users.js";
+import { jobs } from "./jobs.js";
+import { data } from "./data.js";
+import { upload } from "./upload.js";
 
 export const api = express.Router();
 
@@ -18,3 +21,7 @@ api.use("/login", login);
 api.use("/logout", logout);
 api.use("/job-types", jobTypes)
 api.use("/users", users);
+api.use("/jobs", jobs);
+api.use("/data", data);
+api.use("/upload", upload);
+
